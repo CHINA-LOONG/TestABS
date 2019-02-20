4a35ea66c4e252706f77c252c53779a44bddb603
DESKTOP-N9OOS8B\xuelo <xuelong9009@qq.com>
2019-02-20 14:15:55 +0800

    创建项目
    创建项目

---
+++ Assets/Editor/Example.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class Example : MonoBehaviour
+{
+    //Creates a new menu (Build Asset Bundles) and item (Normal) in the Editor
+    [MenuItem("Build Asset Bundles/Normal")]
+    static void BuildABsNone()
+    {
+        //Create a folder to put the Asset Bundle in.
+        // This puts the bundles in your custom folder (this case it's "MyAssetBuilds") within the Assets folder.
+        //Build AssetBundles with no special options
+        BuildPipeline.BuildAssetBundles("Assets/MyAssetBuilds", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+    }
+
+    //Creates a new item (Deterministic) in the new Build Asset Bundles menu
+    [MenuItem("Build Asset Bundles/Deterministic ")]
+    static void BuildABsDeterministic()
+    {
+        //Build the AssetBundles in this mode
+        BuildPipeline.BuildAssetBundles("Assets/MyAssetBuilds", BuildAssetBundleOptions.DeterministicAssetBundle, BuildTarget.StandaloneWindows64);
+    }
+}
